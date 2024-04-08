6b0277d5ad11f51d0a8ef1d8b544879fb749c5dd
Mooling0602 <147964268+Mooling0602@users.noreply.github.com>
2024-04-08 20:06:39 +0800

    Add files via upload

--- matrix_sync/__init__.py
+++ matrix_sync/__init__.py
@@ -1,4 +1,4 @@
-# 导入所需的API
+# Import needed APIs.
 import asyncio
 import json
 import re
@@ -11,21 +11,22 @@ from nio import AsyncClient, LoginResponse, MatrixRoom, RoomMessageText
 
 psi = ServerInterface.psi()
 
-# 默认配置内容
+# Default config.
 default_config = {
     "homeserver": "https://matrix.example.org",
     "user_id": "@username:matrix.example.org",
     "password": "your_password",
-    "room_id": "!your-room_id:matrix.example.org"
+    "room_id": "!your-room_id:matrix.example.org",
+    "user_name": "user-display-name"
 }
 
-# 加载插件并初始化配置文件
+# Load plugin and init default config.
 def on_load(server: PluginServerInterface, old):
     global config, DATA_FOLDER, DATA_FILE, homeserver
 
     config = server.load_config_simple("config.json", default_config)
     DATA_FOLDER = server.get_data_folder()
-    server.logger.info(f"配置文件及登录缓存数据路径: {DATA_FOLDER}")
+    server.logger.info(f"Config path: {DATA_FOLDER}")
     DATA_FILE = f"{DATA_FOLDER}/token.json"
     homeserver = config["homeserver"]
     if not (homeserver.startswith("https://") or homeserver.startswith("http://")):
@@ -33,15 +34,19 @@ def on_load(server: PluginServerInterface, old):
 
     check_config(server)
 
-# 检查配置文件
+# Check the config.
 def check_config(server: PluginServerInterface):
-    if config["homeserver"] == "https://matrix.example.org" or config["user_id"] == "@username:matrix.example.org" or config["password"] == "your_password" or config["room_id"] == "!your-room_id:matrix.example.org":
-        server.logger.info("请修改好所有配置项，然后重新加载插件! ")
+    if "user_name" not in config:
+        server.logger.info("Config needs to be updated manually.")
+        server.logger.info("Add an option user_name in the config and set your bot's display name in this new option.")
+        server.unload.plugin("matrix_sync")
+    if config["homeserver"] == "https://matrix.example.org" or config["user_id"] == "@username:matrix.example.org" or config["password"] == "your_password" or config["room_id"] == "!your-room_id:matrix.example.org" or config["user_name"] == "user-display-name":
+        server.logger.info("Edit default config and reload plugin!")
         server.unload_plugin("matrix_sync")
     else:
-        server.logger.info("正在应用当前配置，请稍后...")
+        server.logger.info("Applying precent config, please wait...")
 
-# 写入缓存
+# Cache data.
 def cache_data(resp: LoginResponse):
     with open(DATA_FILE, "w") as f:
         json.dump(
@@ -51,10 +56,10 @@ def cache_data(resp: LoginResponse):
             f,
         )
     
-# 初始化Matrix机器人
+# Init Matrix bot.
 async def init_client(server: PluginServerInterface) -> None:
     if not os.path.exists(DATA_FILE):
-        server.logger.info("检测到首次登录, 使用账号密码登录中...")
+        server.logger.info("First login, continuing with password...")
         user_id = config["user_id"]
         password = config["password"]
         
@@ -62,13 +67,13 @@ async def init_client(server: PluginServerInterface) -> None:
         resp = await client.login(password, device_name="matrix-nio")
         
         if isinstance(resp, LoginResponse):
-            server.logger.info("登录成功, 正在写入缓存以供稍后和下次登录使用...")
+            server.logger.info("Login successfully, caching data for later use...")
             cache_data(resp)
-            server.logger.info("缓存写入完成! ")
+            server.logger.info("Cache finished! ")
         else:
-            server.logger.info(f"机器人登录失败: {resp}")
-            server.logger.info(f'根服务器: "{homeserver}", 登录用户: "{user_id}"')
-            server.logger.info("请检查账号密码信息是否正确, 并确认网络是否正常, 若有问题可以发issue获取帮助.")
+            server.logger.info(f"Bot login failed: {resp}")
+            server.logger.info(f'Homeserver: "{homeserver}", Account: "{user_id}"')
+            server.logger.info("Please check your account, password and network conditions, you can issue in GitHub for any help.")
             sys.exit(1)
 
     else:
@@ -81,7 +86,7 @@ async def init_client(server: PluginServerInterface) -> None:
         client.device_id = "matrix-nio"
         room_id = config["room_id"]
 
-        message = "MC服务器已启动！"
+        message = "MC Server started successfully! "
         
         await client.room_send(
             room_id,
@@ -92,11 +97,11 @@ async def init_client(server: PluginServerInterface) -> None:
         global test_status
         test_status = True
         if test_status:
-            server.logger.info("机器人登录成功，已向群组发送测试消息.")
+            server.logger.info("Bot login successfully, sent a test message.")
     await client.close()
 
 
-# 检测连接和登录情况，并反馈状态
+# Check connection and login, feedback status.
 def on_server_startup(server: PluginServerInterface):
     asyncio.run(init_client(server))
     if test_status:
@@ -104,8 +109,8 @@ def on_server_startup(server: PluginServerInterface):
 
         
 def on_user_info(server: PluginServerInterface, info: Info):
-        # server.logger.info("检测到玩家消息, 正在尝试发送到Matrix群组...")
-        # 取消上面的注释以判断线上的游戏消息是否开始上报
+        # server.logger.info("Player message detected, trying to send to Matrix group...")
+        # Uncomment the above to determine whether game messages have started to be reported.
         global message
         message = f"<{info.player}> {info.content}"
         if info.player is None:
@@ -116,7 +121,7 @@ def on_user_info(server: PluginServerInterface, info: Info):
         if flag:
             asyncio.run(send_msg(server))
 
-# 消息上报器
+# Message reporter.
 async def send_msg(server: PluginServerInterface) -> None:
     async with aiofiles.open(DATA_FILE, "r") as f:
         contents = await f.read()
@@ -135,13 +140,13 @@ async def send_msg(server: PluginServerInterface) -> None:
 
     await client.close()
 
-# i18n将在正式版本v1.1.0以后使用
-# Translations will be available in ver 1.1.0 and later.
+# i18n will be available in ver 1.1.0 and later.
 
-# 消息接收器
+# Message receiver.
 async def message_callback(room: MatrixRoom, event: RoomMessageText) -> None:
     room_msg = f"[MatrixSync] {room.user_name(event.sender)}: {event.body}"
-    psi.broadcast(f"{room_msg}")
+    if not room.user_name(event.sender) == config["user_name"]:
+        psi.broadcast(f"{room_msg}")
 
 async def get_msg() -> None:
     async with aiofiles.open(DATA_FILE, "r") as f:
@@ -154,7 +159,7 @@ async def get_msg() -> None:
     room_id = config["room_id"]
 
     client.add_event_callback(message_callback, RoomMessageText)
-    await client.sync_forever(timeout=0)
+    await client.sync_forever(timeout=300000)
 
 def on_server_stop(server: PluginServerInterface, server_return_code: int):
     if server_return_code != 0:
