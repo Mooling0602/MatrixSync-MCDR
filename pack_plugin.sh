a725c17d11a1c7ffd8fc34c1e74e67005780057d
Mooling0602 <clemooling@outlook.com>
2024-11-10 13:43:42 +0800

    ci: 子包打包初始化

--- pack_plugin.sh
+++ pack_plugin.sh
@@ -17,7 +17,8 @@ config_file="config.ini"
 framework_ver=$(package_config "$config_file" "framework" "ver")
 main_ver=$(package_config "$config_file" "main" "ver")
 is_stable=$(package_config "$config_file" "release" "test")
-build_plg=$(package_config "$config_file" "ci" "build")
+build_plg=$(package_config "$config_file" "ci" "plugin")
+build_subpack=$(package_config "$config_file" "ci" "subpack")
 
 if [ "$build_plg" -eq 1 ]; then
     if [ "$is_stable" -eq 1 ]; then
@@ -25,4 +26,15 @@ if [ "$build_plg" -eq 1 ]; then
     else
         zip -r "MatrixSync-v${main_ver}.mcdr" README.md README_en_us.md mcdreforged.plugin.json requirements.txt LICENSE lang matrix_sync
     fi
+fi
+if [ "$build_subpack" -eq 1 ]; then
+    pushd "subpacks(rolling)/[MSync]AutoReply" >> /dev/null
+    zip -r "MSync.AutoReply.mcdr" msync_auto_reply mcdreforged.plugin.json requirements.txt
+    popd >> /dev/null
+    pushd "subpacks(rolling)/[MSync]MoreMessages" >> /dev/null
+    zip -r "MSync.MoreMessages.mcdr" msync_more_messages mcdreforged.plugin.json
+    popd >> /dev/null
+    pushd "subpacks(rolling)/[MSync]PlayingTips" >> /dev/null
+    zip -r "MSync.PlayingTips.mcdr" msync_playing_tips mcdreforged.plugin.json
+    popd >> /dev/null
 fi
